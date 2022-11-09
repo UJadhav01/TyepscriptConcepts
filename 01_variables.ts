2e573b239b339fa70772e1a8ad937dd3abc85b91
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2022-11-09 20:17:21 +0530

    variable.ts

---
+++ 01_variables.ts
@@ -0,0 +1,21 @@
+var num:number=10;
+console.log(num);
+
+//string
+
+let string1="Hello welcome to typescript";
+let string2=`You are awesome`;
+console.log(string1);
+console.log(string2);
+
+//boolean
+let b1:boolean=true;
+let b2:boolean=false;
+let b3:boolean;
+
+function test():boolean{
+return b3=b1;
+    console.log(b3); //unreachable screen
+}
+console.log(test());
+
