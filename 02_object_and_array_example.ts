09dc99920a463a7d5c3bb992b8627863ff950e35
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2022-11-09 20:17:59 +0530

    object and array.ts

---
+++ 02_object_and_array_example.ts
@@ -0,0 +1,16 @@
+//creating object
+let a1:any={
+    productId:1,
+    productName:'iPhone',
+    productPrice:56000,
+}
+console.log(a1);
+
+//array which stores only numbers
+let array:Array<number>=[2,3,5,6,7,8];
+console.log(array);
+
+
+//array which stores any type of data
+let array2:Array<any>=['typescript',2,true];
+console.log(array2);
