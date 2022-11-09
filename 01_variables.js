6babaa8fb30ce032182dca36a5fffcf36ae520c8
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2022-11-09 20:17:13 +0530

    variable.js

---
+++ 01_variables.js
@@ -0,0 +1,16 @@
+var num = 10;
+console.log(num);
+//string
+var string1 = "Hello welcome to typescript";
+var string2 = "You are awesome";
+console.log(string1);
+console.log(string2);
+//boolean
+var b1 = true;
+var b2 = false;
+var b3;
+function test() {
+    return b3 = b1;
+    console.log(b3);
+}
+console.log(test());
