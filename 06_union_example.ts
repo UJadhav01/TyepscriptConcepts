612b3a8e5b4fdccbd166cba3aa1e7354737a5825
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2022-11-09 20:20:18 +0530

    union.ts

---
+++ 06_union_example.ts
@@ -0,0 +1,7 @@
+let s:string | number; // union s which takes value as string or number
+s='typescript';
+console.log(s);
+s=10;
+console.log(s);
+
+//s=true; because union s consider only string and number
