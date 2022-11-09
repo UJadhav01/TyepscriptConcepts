6796420428e603e71c905e31908a4cb17ca91066
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2022-11-09 20:19:38 +0530

    stringType.ts

---
+++ 05_stringType.ts
@@ -0,0 +1,6 @@
+let username:string='Codemind Technology';
+let myName=`My name is ${username}`;
+console.log(myName);
+
+username=`My name is ${username}`;// just update var username it will return new string always
+console.log(username);
