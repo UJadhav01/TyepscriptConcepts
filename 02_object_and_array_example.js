c91ccdaec2574656a3cc8fc33c17033481bceec0
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2022-11-09 20:17:48 +0530

    object array.js

---
+++ 02_object_and_array_example.js
@@ -0,0 +1,13 @@
+//creating object
+var a1 = {
+    productId: 1,
+    productName: 'iPhone',
+    productPrice: 56000
+};
+console.log(a1);
+//array which stores only numbers
+var array = [2, 3, 5, 6, 7, 8];
+console.log(array);
+//array which stores any type of data
+var array2 = ['typescript', 2, true];
+console.log(array2);
