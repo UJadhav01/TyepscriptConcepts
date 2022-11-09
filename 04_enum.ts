98ecaea6dc9f9f422d992caa28f083ff8665842d
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2022-11-09 20:18:59 +0530

    enum.ts

---
+++ 04_enum.ts
@@ -0,0 +1,18 @@
+enum Gender{
+    Male,
+    Female
+}
+console.log(Gender.Male);
+console.log(Gender.Female);
+console.log(Gender[0]);
+console.log(Gender[1]);
+
+//enum for weekends
+enum Weekend{
+Saturday=6, //default value is 6
+Sunday=7 
+}
+console.log(Weekend[6]);
+console.log(Weekend[7]);
+
+
